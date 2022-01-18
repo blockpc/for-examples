f325296fa4cbc3e1e1fefeda9042341f1fcbd55e
Juan Carlos Marchant <juan.marchant@gmail.com>
2022-01-18 15:04:28 -0300

    add vehicle example

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('vehicles')" :active="request()->routeIs('vehicles')">
+                        {{ __('Vehicles') }}
+                    </x-nav-link>
                 </div>
             </div>
 
